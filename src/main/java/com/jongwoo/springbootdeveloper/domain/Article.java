b5894f937d31d067219aec4849169eb02e24a435
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-11 18:35:41 +0900

    feat: domain 패키지에 Article 클래스 구현

---
+++ src/main/java/com/jongwoo/springbootdeveloper/domain/Article.java
@@ -0,0 +1,23 @@
+package com.jongwoo.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
