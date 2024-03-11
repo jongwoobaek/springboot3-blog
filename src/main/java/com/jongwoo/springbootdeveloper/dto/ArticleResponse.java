5b7ef49857c6b87da7a682c3119cf1e6aa5d4f88
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-11 19:46:32 +0900

    feat: dto 패키지에 ArticleResponse 클래스 구현

---
+++ src/main/java/com/jongwoo/springbootdeveloper/dto/ArticleResponse.java
@@ -0,0 +1,15 @@
+package com.jongwoo.springbootdeveloper.dto;
+
+import com.jongwoo.springbootdeveloper.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleResponse {
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
