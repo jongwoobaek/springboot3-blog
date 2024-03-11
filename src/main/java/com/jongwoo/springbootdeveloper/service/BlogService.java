33fecba6f589f6c30eea016542736133acf192d2
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-12 00:21:39 +0900

    feat: BlogService 클래스 update 메서드 구현

--- src/main/java/com/jongwoo/springbootdeveloper/service/BlogService.java
+++ src/main/java/com/jongwoo/springbootdeveloper/service/BlogService.java
@@ -2,9 +2,11 @@ package com.jongwoo.springbootdeveloper.service;
 
 import com.jongwoo.springbootdeveloper.domain.Article;
 import com.jongwoo.springbootdeveloper.dto.AddArticleRequest;
+import com.jongwoo.springbootdeveloper.dto.UpdateArticleRequest;
 import com.jongwoo.springbootdeveloper.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -28,5 +30,15 @@ public class BlogService {
 
     public void deleteById(long id) {
         blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
